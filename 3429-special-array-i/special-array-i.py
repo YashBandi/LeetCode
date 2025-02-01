c0e0e6a6fdd0d30008de99ad6954cf5fb578828b
YashBandi <126181936+YashBandi@users.noreply.github.com>
2025-02-01 12:35:21 -0500

    Time: 1 ms (35.36%) | Memory: 18 MB (12.54%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -1,15 +1,8 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        temp = []
         if len(nums) < 2:
             return True
         for i in range(1,len(nums)):
-            if (nums[i-1]%2 == 0 and nums[i]%2 != 0) or (nums[i-1]%2 != 0 and nums[i]%2 == 0):
-                temp.append(1)
-            else:
-                temp.append(0)
-            # i += 1
-        res = all(temp)
-        return res
-
-        
+            if nums[i-1]%2 == nums[i]%2:    #Just checking no 2 adjacent elements are both even or both odd. If both are even then % gives 0, if both odd % gives 1. So, return false if 0 == 0 or 1 == 1.
+                return False
+        return True
