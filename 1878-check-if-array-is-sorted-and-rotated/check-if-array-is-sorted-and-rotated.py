d0d7234fc44fc9c4a69addbe62abfbc670b48e21
YashBandi <126181936+YashBandi@users.noreply.github.com>
2025-02-02 01:27:54 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.79%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,15 +1,9 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
         c = 0
-        # nums = list(dict.fromkeys(nums))    #Removing duplicates from the nums
-        #print(nums)
         for i in range(len(nums)-1):
             if nums[i]>nums[i+1]:
                 c +=1 
-        #nums = list(dict.fromkeys(nums))
-        print(c)
-        print(nums[len(nums)-1])
-        print(nums[0])
         if c == 1 and (nums[len(nums)-1] <= nums[0]):
             return True
         elif c == 0:
