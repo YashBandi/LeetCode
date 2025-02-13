bbb13293be4b8644852f04a917ba74bd845efaf6
YashBandi <126181936+YashBandi@users.noreply.github.com>
2025-02-12 20:06:47 -0500

    Time: 249 ms (52.94%) | Memory: 35.4 MB (91.60%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -1,11 +1,23 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        ans = 0
-        arr = SortedList(nums)
-        while len(arr) > 1 and k > arr[0]:
-            ans += 1
-            arr.add((arr[0] * 2) + arr[1])
-            arr.discard(arr[1])
-            arr.discard(arr[0])
-        return ans
+        heapq.heapify(nums)
+        val = 0
+
+        while nums[0] < k:
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+            heapq.heappush(nums, min(x, y) * 2 + max(x, y))
+            val += 1
+
+        return val
+
+        # Solution 2
+        # ans = 0
+        # arr = SortedList(nums)
+        # while len(arr) > 1 and k > arr[0]:
+        #     ans += 1
+        #     arr.add((arr[0] * 2) + arr[1])
+        #     arr.discard(arr[1])
+        #     arr.discard(arr[0])
+        # return ans
         
