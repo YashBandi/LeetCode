ece7e399b69c71f4ed3210ace343341ad5ec6057
YashBandi <126181936+YashBandi@users.noreply.github.com>
2025-02-22 01:37:20 -0500

    Time: 1019 ms (58.12%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-SELECT tweet_id FROM Tweets where CHAR_LENGTH(content)>15;
+select tweet_id from Tweets where length(content) >15 
+-- or
+-- SELECT tweet_id FROM Tweets where CHAR_LENGTH(content)>15;
