51c5c8f0fbf84ca3cca1aa4d38fba497638e5831
YashBandi <126181936+YashBandi@users.noreply.github.com>
2025-03-07 00:35:49 -0500

    Time: 1106 ms (30.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select p.project_id, ifnull(round(sum(e.experience_years)/count(p.project_id),2),0) as average_years from Project p join Employee e on p.employee_id = e.employee_id group by project_id
