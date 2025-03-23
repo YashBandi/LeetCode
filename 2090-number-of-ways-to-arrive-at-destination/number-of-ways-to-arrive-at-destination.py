8929b34c28bb87ff5493da9c51ea163c4401c8e3
YashBandi <126181936+YashBandi@users.noreply.github.com>
2025-03-23 00:07:41 -0400

    Time: 15 ms (90.31%) | Memory: 25.4 MB (28.75%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
@@ -1,25 +1,25 @@
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for u, v, t in roads:
-            adj[u].append((t, v))
-            adj[v].append((t, u))
-        dist = [(0, 0, -1)] # time, node, previous
-        vis = set()
-        ways = [0] * (n + 1) # ways to reach each node
-        ways[-1] = 1
-        while dist:
-            time, node, prev = heappop(dist)
-            if node in vis:
-                continue
-            vis.add(node)
-            ways[node] += ways[prev]
-            while dist and dist[0][0] == time and dist[0][1] == node: 
-                _, _, p = heappop(dist)
-                ways[node] += ways[p]
-            if node == n-1:
-                return ways[node] % (10 ** 9 + 7)
-            for t, nd in adj[node]:
-                if nd not in vis:
-                    heappush(dist, (t + time, nd, node))
-        
+        graph = defaultdict(list)
+        for u, v, time in roads:
+            graph[u].append((v, time))
+            graph[v].append((u, time))
+
+        ways = [0]*n
+        ways[0] = 1
+        time = [float('inf')]*n
+        time[0] = 0
+        min_heap = [(0, 0)]
+
+        while min_heap:
+            time1, src = heapq.heappop(min_heap) 
+            for dst, time2 in graph[src]:
+                if time1+time2 < time[dst]:
+                    time[dst] = time1+time2
+                    ways[dst] = ways[src]
+                    heapq.heappush(min_heap, (time[dst], dst))
+                elif time1+time2 == time[dst]:
+                    ways[dst] += ways[src]
+
+        if ways[n-1] == float('inf'): return -1
+        return ways[n-1]% (pow(10, 9) + 7 )  
